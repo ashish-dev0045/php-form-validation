d14da213fce84e5f44701319de48cdbd0273a210
Ashish gupta <gupta.ashish0045@gmail.com>
2024-07-26 12:56:23 +0530

    change

--- handson form validation/success.php
+++ handson form validation/success.php
@@ -1,7 +1,7 @@
 
 <?php 
 session_start();
-echo "Your name is ".$_SESSION['name']."</br>";
+echo "Your name is ".$_SESSION['name']."</br> ";
 echo "Your email is ".$_SESSION['email']."</br>";
 echo "Your gender is ".$_SESSION['gender']."</br>";
 echo "Your interest is ".$_SESSION['interest']."</br>";
